a18692049793a92010ae89e09b5065f24491d12b
ggg05158 <csg05158@naver.com>
2020-08-05 20:32:39 +0900

    두원의 접점구하기

--- 1002.c
+++ 1002.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -6,31 +7,33 @@ int main()
 {
 	double x1, y1, x2, y2, r1, r2;
 	int T;
-	scanf_s("%d", &T);
-	int* answer;
-	answer = (int*)malloc(sizeof(int) * T);
+	scanf("%d", &T);
 	for (int i = 0; i < T; i++)
 	{
 		scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &r1, &x2, &y2, &r2);
 		double len = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-		if (x1 == x2 && y1 == y2) {
+		if (x1 == x2 && y1 == y2)
+		{
 			if (r1 == r2)
-				answer[i]=-1;
+				printf("-1\n");
 			else
-				answer[i] = 0;
+				printf("0\n");
+
 		}
-		else {
-			if (len > (r1 + r2))
-				answer[i] = 0;
-			else if (len == (r1 + r2))
-				answer[i] = 1;
+		else if ((r1 > r2 ? r1 : r2) >= len + (r1 > r2 ? r2 : r1))
+		{
+			if ((r1 > r2 ? r1 : r2) == len + (r1 > r2 ? r2 : r1))
+				printf("1\n");
 			else
-				answer[i] = 2;
+				printf("0\n");
 		}
+		else if (len > (r1 + r2))
+			printf("0\n");
+		else if (len == (r1 + r2))
+			printf("1\n");
+		else
+			printf("2\n");
+
 	}
-	for (int j = 0; j < T; j++) {
-		printf("%d\n", answer[j]);
-	}
-	free(answer);
 	return 0;
 }
